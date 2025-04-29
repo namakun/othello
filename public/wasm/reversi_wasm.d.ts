d84a03d656317a55fbf105473c16849100ec4049
namakun <t.hiroki9211@gmail.com>
2025-04-29 22:51:05 +0900

    wasmのファイル追加

---
+++ public/wasm/reversi_wasm.d.ts
@@ -0,0 +1,67 @@
+/* tslint:disable */
+/* eslint-disable */
+export function init_panic_hook(): void;
+/**
+ * 合法手ビットボード生成
+ */
+export function gen_legal_moves(p: bigint, o: bigint): bigint;
+/**
+ * 方向別「反転ビットマスク」を Vec<u64> で返す（旧 API）
+ * TODO Deprecate
+ */
+export function gen_flip_bitboards(p: bigint, o: bigint, pos: number): BigUint64Array;
+/**
+ * ★新 API: 方向別「反転 index 配列」を Array<Uint8Array> で返す
+ */
+export function gen_flip_groups(p: bigint, o: bigint, pos: number): Array<any>;
+/**
+ * 着手適用（反転も含めた次盤面を返す）
+ */
+export function apply_move(p: bigint, o: bigint, pos: number): BigUint64Array;
+/**
+ * 石数 (スコア) を数える
+ */
+export function popcnt64(x: bigint): number;
+/**
+ * 合法手が存在するか
+ */
+export function has_moves(p: bigint, o: bigint): boolean;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly init_panic_hook: () => void;
+  readonly gen_legal_moves: (a: bigint, b: bigint) => bigint;
+  readonly gen_flip_bitboards: (a: bigint, b: bigint, c: number) => [number, number];
+  readonly gen_flip_groups: (a: bigint, b: bigint, c: number) => any;
+  readonly apply_move: (a: bigint, b: bigint, c: number) => [number, number];
+  readonly popcnt64: (a: bigint) => number;
+  readonly has_moves: (a: bigint, b: bigint) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_export_3: WebAssembly.Table;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ public/wasm/reversi_wasm_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const init_panic_hook: () => void;
+export const gen_legal_moves: (a: bigint, b: bigint) => bigint;
+export const gen_flip_bitboards: (a: bigint, b: bigint, c: number) => [number, number];
+export const gen_flip_groups: (a: bigint, b: bigint, c: number) => any;
+export const apply_move: (a: bigint, b: bigint, c: number) => [number, number];
+export const popcnt64: (a: bigint) => number;
+export const has_moves: (a: bigint, b: bigint) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export const __wbindgen_start: () => void;
